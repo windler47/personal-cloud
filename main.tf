a2ea35583ae3f15f29523627a39b85cf40cf56df
Aleksandr Maksimov <coffeeman@yandex-team.ru>
2024-08-02 11:26:52 +0200

    add cloud-init

--- main.tf
+++ main.tf
@@ -1,70 +1,80 @@
+locals {
+  target_folder_id = "b1g8glslhvn0od004lvq"
+  zone = "ru-central1-d"
+}
+
 terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = ">= 0.47.0"
     }
   }
-  required_version = ">= 0.77"
-  
   backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "windler47-terraform-object-storage"
-    region     = "ru-central1"
-    key        = "terraform/windler47-personal-cloud.tfstate"
+    endpoints = {
+      s3        = "https://storage.yandexcloud.net"
+      dynamodb  = "https://docapi.serverless.yandexcloud.net/ru-central1/b1ghcdiesvi7hvcfvag5/etnp2pgupsoj2419q6or"
+    }
+    bucket = "windler47-tf"
+    region = "ru-central1"
+    key    = "windler47.tfstate"
+    
+    dynamodb_table = "state-lock-table"
+
     skip_region_validation      = true
     skip_credentials_validation = true
-  }
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  } 
 }
 
 provider "yandex" {
-  token     = var.yandex_oauth_token
-  cloud_id  = var.yandex_cloud_id
-  folder_id = var.yandex_folder_id
-  zone      = var.yandex_availability_zone
+  zone = local.zone
+  service_account_key_file = "key.json"
+  folder_id  = local.target_folder_id
 }
 
-resource "yandex_vpc_network" "default" {
-  name = "default"
+data "yandex_compute_image" "ubuntu-2404-lts-oslogin" {
+  family = "ubuntu-2404-lts-oslogin"
 }
 
-resource "yandex_vpc_subnet" "default-ru-central1-a" {
-  zone           = var.yandex_availability_zone
-  network_id     = "${yandex_vpc_network.default.id}"
-  name           = "default-ru-central1-a"
-  v4_cidr_blocks = ["10.128.0.0/24"]
+# data "yandex_vpc_network" "network_default" {
+#   name = "default"
+# }
+
+data "yandex_vpc_subnet" "default-ru-central1-d" {
+  name = "default-ru-central1-d"
 }
 
-resource "yandex_vpc_address" "wg-ru-1-public-adress" {
-  name = "wg public ip adress"
-
-  external_ipv4_address {
-    zone_id = "ru-central1-a"
-  }
+resource "yandex_compute_disk" "wireguard-out-disk" {
+  name     = "wireguard-out-disk"
+  type     = "network-hdd"
+  size     = "10"
+  image_id = data.yandex_compute_image.ubuntu-2404-lts-oslogin.id
 }
 
-resource "yandex_compute_instance" "wg-ru-1" {
-  ## https://cloud.yandex.ru/docs/compute/concepts/vm-platforms
-  platform_id = "standard-v2"  # Intel Cascade Lake
+resource "yandex_compute_instance" "wireguard-out" {
+  # https://yandex.cloud/ru/docs/compute/concepts/performance-levels
+  platform_id = "standard-v2"
   resources {
-    cores  = 2
-    memory = 0.5
-    core_fraction = 5
+    core_fraction = "20"
+    cores  = "2"
+    memory = "1"
   }
-
+  
   boot_disk {
-    initialize_params {
-      image_id = var.yandex_base_image_id
-    }
+    disk_id = yandex_compute_disk.wireguard-out-disk.id
   }
 
   network_interface {
-    subnet_id = "${yandex_vpc_subnet.default-ru-central1-a.id}"
-    nat = true
-    nat_ip_address = "${yandex_vpc_address.wg-ru-1-public-adress.external_ipv4_address[0].address}"
+    subnet_id = data.yandex_vpc_subnet.default-ru-central1-d.id
+    nat       = true
   }
+  
+  name                      = "wireguard-out"
+  hostname                  = "wireguard-out"
 
-   metadata = {
-    ssh-keys = "ubuntu:${file("id_ecdsa.pub")}"
+  metadata = {
+    user-data = "${file("cloud-init.yaml")}"
   }
-
 }
